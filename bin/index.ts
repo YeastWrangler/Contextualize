1d1d0f66fef877c58776dd5eafa78731d752ee90
Alex Rattray <102862775+YeastWrangler@users.noreply.github.com>
2023-11-08 19:55:44 -0600

    Update index.ts

--- bin/index.ts
+++ bin/index.ts
@@ -2,20 +2,61 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsStack } from '../lib/aws-stack';
+import * as elasticache from 'aws-cdk-lib/aws-elasticache';
+import * as ec2Instance from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPattern from 'aws-cdk-lib/aws-ecs-patterns';
+
 
 const app = new cdk.App();
-new AwsStack(app, 'AwsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const stack = new cdk.Stack(app, 'FlaskWebAppStack', {
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  
 });
+
+
+const vpc = new ec2Instance.Vpc(stack, 'FlaskWebAppVPC', {
+  maxAzs: 2,
+});
+
+const ecsCluster = new ecs.Cluster(stack, 'FlaskWebAppCluster', {
+  vpc,
+});
+
+const redisCluster = new elasticache.CfnCacheCluster(stack, 'RedisCluster', {
+  cacheNodeType: 'cache.m3.medium',
+  engine: 'redis',
+  numCacheNodes: 1,
+  vpcSecurityGroupIds: [vpc.vpcDefaultSecurityGroup]
+  
+});
+
+const flaskAppImage = ecs.ContainerImage.fromRegistry('FLASK_IMAGE');
+
+const flaskAppService = new ecsPattern.ApplicationLoadBalancedFargateService(stack, 'FlaskAppService', {
+  cluster: ecsCluster,
+  taskImageOptions: {
+    image: flaskAppImage,
+  },
+  publicLoadBalancer: true,
+  desiredCount: 2,
+});
+
+const nginxProxyTaskDef = new ecs.FargateTaskDefinition(stack, 'NginxProxyTask');
+
+const nginxContainer = nginxProxyTaskDef.addContainer('nginx-proxy', {
+  image: ecs.ContainerImage.fromRegistry('nginx'),
+  portMappings: [{ containerPort: 80 }],
+});
+
+const nginxProxyService = new ecs.FargateService(stack, 'NginxProxyService', {
+  cluster: ecsCluster,
+  taskDefinition: nginxProxyTaskDef,
+  desiredCount: 1,
+});
+
+app.synth();
